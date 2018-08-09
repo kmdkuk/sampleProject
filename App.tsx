ba8af73b9853df91c43a324157102b21359fece9
kouki <kouworld0123@gmail.com>
2018-08-09 18:01:54 +0900

    https://facebook.github.io/react-native/docs/height-and-width

--- App.tsx
+++ App.tsx
@@ -10,17 +10,87 @@ import React from 'react';
 import { Component } from 'react';
 import {
   Text,
-  View
+  View,
+  Image,
+  StyleSheet,
 } from 'react-native';
+interface Props { }
+interface GreetingProps {
+  name: string,
+}
+interface BlinkProps {
+  text: string,
+}
+interface BlinkState {
+  isShowingText: boolean,
+}
 
-type Props = {};
-export default class App extends Component<Props> {
-  foo: string = "a";
+class Blink extends Component<BlinkProps, BlinkState> {
+  constructor(props: any) {
+    super(props);
+    this.state = { isShowingText: true };
+
+    setInterval(() => {
+      this.setState(previousState => {
+        return { isShowingText: !previousState.isShowingText };
+      });
+    }, 1000);
+  }
+
   render() {
+    let display: string = this.state.isShowingText ? this.props.text : ' ';
     return (
-      <View>
-
-      </View>
+      <Text>{display}</Text>
     );
   }
 }
+
+class Greeting extends Component<GreetingProps> {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+export default class App extends Component<Props> {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    }
+    return (
+      <View style={{ alignItems: 'center' }}>
+        <View style={{ flex: 1, backgroundColor: 'powderblue' }} />
+        <View style={{ flex: 2, backgroundColor: 'skyblue' }} />
+        <View style={{ flex: 3, backgroundColor: 'steelblue' }} />
+        <Text>Hello world!</Text>
+        <Image source={pic} style={{ width: 193, height: 110 }} />
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigblue}>just bigblue</Text>
+        <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+        <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+        <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
+        <View style={{ width: 100, height: 100, backgroundColor: 'skyblue' }} />
+        <View style={{ width: 150, height: 150, backgroundColor: 'steelblue' }} />
+      </View >
+    );
+  }
+}
+
+const styles: any = StyleSheet.create({
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+})

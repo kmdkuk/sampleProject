0b7875cd79f77a8a6a9af3e8385e9f1b777f99e7
kouki <kouworld0123@gmail.com>
2018-08-09 18:44:59 +0900

    https://facebook.github.io/react-native/docs/using-a-listview

--- App.tsx
+++ App.tsx
@@ -9,96 +9,25 @@
 import React from 'react';
 import { Component } from 'react';
 import {
-  Alert,
-  Platform,
+  SectionList,
   StyleSheet,
   Text,
-  TouchableHighlight,
-  TouchableOpacity,
-  TouchableNativeFeedback,
-  TouchableWithoutFeedback,
   View
 } from 'react-native';
-interface Props { }
-interface GreetingProps {
-  name: string,
-}
-interface BlinkProps {
-  text: string,
-}
-interface State {
-  text: string,
-}
-interface BlinkState {
-  isShowingText: boolean,
-}
 
-class Blink extends Component<BlinkProps, BlinkState> {
-  constructor(props: any) {
-    super(props);
-    this.state = { isShowingText: true };
-
-    setInterval(() => {
-      this.setState(previousState => {
-        return { isShowingText: !previousState.isShowingText };
-      });
-    }, 1000);
-  }
-
-  render() {
-    let display: string = this.state.isShowingText ? this.props.text : ' ';
-    return (
-      <Text>{display}</Text>
-    );
-  }
-}
-
-class Greeting extends Component<GreetingProps> {
-  render() {
-    return (
-      <Text>Hello {this.props.name}!</Text>
-    );
-  }
-}
-
-export default class App extends Component<Props, State> {
-  _onPressButton() {
-    Alert.alert(`You tapped the button`);
-  }
-  _onLongPressButton() {
-    Alert.alert('You long-pressed the button!')
-  }
+export default class SectionListBasics extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableHighlight</Text>
-          </View>
-        </TouchableHighlight>
-        <TouchableOpacity onPress={this._onPressButton}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableOpacity</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableNativeFeedback
-          onPress={this._onPressButton}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableNativeFeedback</Text>
-          </View>
-        </TouchableNativeFeedback>
-        <TouchableWithoutFeedback
-          onPress={this._onPressButton}
-        >
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
-          </View>
-        </TouchableWithoutFeedback>
-        <TouchableHighlight onPress={this._onPressButton} onLongPress={this._onLongPressButton} underlayColor="white">
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>Touchable with Long Press</Text>
-          </View>
-        </TouchableHighlight>
+        <SectionList
+          sections={[
+            { title: 'D', data: ['Devin'] },
+            { title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie'] },
+          ]}
+          renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({ section }) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+          keyExtractor={(index) => index}
+        />
       </View>
     );
   }
@@ -106,17 +35,21 @@ export default class App extends Component<Props, State> {
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 60,
-    alignItems: 'center'
+    flex: 1,
+    paddingTop: 22
   },
-  button: {
-    marginBottom: 30,
-    width: 260,
-    alignItems: 'center',
-    backgroundColor: '#2196F3'
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
   },
-  buttonText: {
-    padding: 20,
-    color: 'white'
-  }
-});
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
